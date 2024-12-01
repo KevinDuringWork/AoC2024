bcd9a78af5e1bbcaea53ecaa5018234c1f667231
Kevin Shi <kshi@nygenome.org>
2024-12-01 15:10:08 -0800

    applied hints

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Text    as Text 
+import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
-import qualified Data.List 
+import qualified Data.List
 
-main = do 
+main :: IO ()
+main = do
 
-   {-- Part 1 [Distance] --}   
+   {-- Part 1 [Distance] --}
   lines <- fmap Text.lines (Text.readFile "input.txt")
-  let first  = Data.List.sort $ fmap((read::String->Int) . Text.unpack . head . Text.words) lines 
-  let second = Data.List.sort $ fmap((read::String->Int) . Text.unpack . last . Text.words) lines
-  let distance = sum $ zipWith (curry(\(x, y) -> abs(x - y))) first second
+  let first  = Data.List.sort $ fmap ((read::String->Int) . Text.unpack . head . Text.words) lines
+  let second = Data.List.sort $ fmap ((read::String->Int) . Text.unpack . last . Text.words) lines
+  let distance = sum $ zipWith (\ x y -> abs (x - y)) first second
 
   {-- Part 2 [Similarity] --}
   let similarity = map (\x -> Data.List.length $ Data.List.filter (==x) second) first
-  let distance2 = sum $ zipWith (curry(\(x, y) -> x * y)) first similarity
+  let distance2 = sum $ zipWith (*) first similarity
 
   print distance
-  print distance2   
-  
+  print distance2
+
