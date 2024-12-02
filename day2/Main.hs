af11504e3a755be0a084feaff1f323ef384b6be4
Kevin Shi <kshi@nygenome.org>
2024-12-02 10:50:36 -0800

    applied optimizations

--- day2/Main.hs
+++ day2/Main.hs
@@ -26,25 +26,26 @@ explodeAll x = map (`explode` x) [1 .. length x]
 main :: IO ()
 main = do
     lines <- fmap Text.lines (Text.readFile "input.txt")
-    let matrix = fmap (fmap ((read::String->Int) . Text.unpack) . Text.words) lines
+    let 
+        matrix = fmap (fmap ((read::String->Int) . Text.unpack) . Text.words) lines
 
-    {-- Calculate changes --}
-    let diff = fmap (\x -> zipWith (-) (drop 1 x) x ) matrix
+        {-- Calculate changes --}
+        diff = fmap (\x -> zipWith (-) (drop 1 x) x ) matrix
 
-    {-- Summarize Changes, i.e, monotonicity, min/max steps --}
-    let sum_matrix = map summary diff
+        {-- Summarize Changes, i.e, monotonicity, min/max steps --}
+        sum_matrix = map summary diff
 
-    {-- Validate Summary to "Safety" and Count --}
-    let safe_list = map safe sum_matrix
-    let part1_answer = length (filter id safe_list)
+        {-- Validate Summary to "Safety" and Count --}
+        safe_list = map safe sum_matrix
+        part1_answer = length (filter id safe_list)
 
-    {-- Part (2): Tolerate a single failure, we must go deeper --}
-    let matrix2 = fmap explodeAll matrix
-    let diff2 = map (map (\x -> zipWith (-) (drop 1 x) x)) matrix2
-    let sum_matrix2 = map (map summary) diff2
-    let safe_list2 = map ((not . null) . filter id . map safe) sum_matrix2
-    let part2 = zip safe_list safe_list2
-    let part2_answer = length (filter id $ map (uncurry (||)) part2)
+        {-- Part (2): Tolerate a single failure, we must go deeper --}
+        matrix2 = fmap explodeAll matrix
+        diff2 = map (map (\x -> zipWith (-) (drop 1 x) x)) matrix2
+        sum_matrix2 = map (map summary) diff2
+        safe_list2 = map ((not . null) . filter id . map safe) sum_matrix2
+        part2 = zip safe_list safe_list2
+        part2_answer = length (filter id $ map (uncurry (||)) part2)
 
     print part1_answer
     print part2_answer
