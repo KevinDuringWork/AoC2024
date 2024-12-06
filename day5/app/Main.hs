24e505b1f57cd48a46df70284d7da22e720f457e
Kevin Shi <kshi@nygenome.org>
2024-12-05 20:04:59 -0800

    cleanup

--- day5/app/Main.hs
+++ day5/app/Main.hs
@@ -6,43 +6,40 @@ import qualified Data.List
 import qualified Data.MultiMap as MultiMap
 import Data.List (elemIndex)
 
-check :: MultiMap.MultiMap String String -> MultiMap.MultiMap String String -> String -> String -> [String] -> Bool
-check a b x c u = forward_check && backward_check
+check :: MultiMap.MultiMap String String -> String -> String -> [String] -> Bool
+check a x c u = c `elem` (a MultiMap.! x) || (elemIndex x u > elemIndex c u)
+
+validate :: MultiMap.MultiMap String String -> String -> [String] -> Bool
+validate a v update = all (\c -> check a v c update) (Data.List.filter (v /=) update)
+
+validateAll :: MultiMap.MultiMap String String -> [[String]] -> [Bool]
+validateAll a = fmap (\u -> all (\v -> validate a v u) u)
+
+stitch :: MultiMap.MultiMap String String -> [String] -> [String]
+stitch _ [] = [] 
+stitch a i = valid : stitch a (Data.List.filter (valid /= ) i)
     where
-        forward_check  = c `elem` (a MultiMap.! x) || (elemIndex x u > elemIndex c u)
-        backward_check = c `elem` (b MultiMap.! x) || (elemIndex x u < elemIndex c u)
-        -- backward_check = True 
-
-validate :: MultiMap.MultiMap String String -> MultiMap.MultiMap String String -> String -> [String] -> Bool
-validate a b v update = all (\c -> check a b v c update) (Data.List.filter (v /=) update)
-
-validateAll :: MultiMap.MultiMap String String -> MultiMap.MultiMap String String -> [[String]] -> [Bool]
-validateAll a b = fmap (\u -> all (\v -> validate a b v u) u)
-
-stitch :: MultiMap.MultiMap String String -> MultiMap.MultiMap String String -> [String] -> [String]
-stitch _ _ [] = [] 
-stitch a b i = valid : stitch a b (Data.List.filter (valid /= ) i)
-    where
-        valid = fst $ head $ filter snd $ fmap (\c -> (c, validate a b c (c : Data.List.filter (c /=) i))) i
+    valid = fst $ head $ filter snd $ 
+        map (\c -> (c, validate a c (c : Data.List.filter (c /=) i))) i
 
 main :: IO()
 main = do
     content <- Text.readFile "input.txt"
+    
     let
-        datum = Text.splitOn "\n\n" content
-        raw_inst = Text.lines $ head datum
+        datum     = Text.splitOn "\n\n" content
+        raw_inst  = Text.lines $ head datum
+        instruct  = MultiMap.fromList ((\s ->  (head s, last s)) . fmap Text.unpack . Text.splitOn "|" <$> raw_inst)
+        updates   = fmap (fmap Text.unpack . Text.splitOn ",") $ Text.lines $ last datum
 
         -- part 1 
-        instruct  = MultiMap.fromList ((\s ->  (head s, last s)) . fmap Text.unpack . Text.splitOn "|" <$> raw_inst)
-        instruct' = MultiMap.fromList ((\s ->  (last s, head s)) . fmap Text.unpack . Text.splitOn "|" <$> raw_inst)
-
-        updates = fmap (fmap Text.unpack . Text.splitOn ",") $ Text.lines $ last datum
-        validated = [fst x | x <- filter snd $ zip updates (validateAll instruct instruct' updates)]
-        computed_answer = sum $ fmap (read::String->Int) [x !! (length x `div` 2) | x <- validated]
-
+        validated = [fst x | x <- filter snd $ zip updates (validateAll instruct updates)]
+        
         -- part 2 
-        invalidated = [fst x | x <- filter (\(_,y) -> not y) $ zip updates (validateAll instruct instruct' updates)]
-        permuted = fmap (stitch instruct instruct') invalidated
+        invalid   = [fst x | x <- filter (\(_,y) -> not y) $ zip updates (validateAll instruct updates)]
+        permuted  = fmap (stitch instruct) invalid
+
+        computed_answer  = sum $ fmap (read::String->Int) [x !! (length x `div` 2) | x <- validated]
         computed_answer2 = sum $ fmap (read::String->Int) [x !! (length x `div` 2) | x <- permuted]
 
     print computed_answer
